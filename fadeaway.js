c426d1484a232c19746200e61283a384d525bb75
Jpegzilla <jpegzilla@gmail.com>
2019-10-01 00:08:30 -0400

    hmm again

--- fadeaway.js
+++ fadeaway.js
@@ -1,4 +1,4 @@
-window.onresize = () => {
+window.addEventListener("resize", () => {
   let d = ((window.innerWidth / 1920) * 100) / 100;
   requestAnimationFrame(() => (document.documentElement.style.opacity = d));
-};
+});
